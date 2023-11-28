e173a20c5c0c0a07341a0b4cb66af6e81034d1c4
Eugene Hnatiuk <gnatyuk.e@gmail.com>
2023-11-28 16:29:46 +0200

    conditions for 3-d and 4-th, variables for 4-th

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-// // За допомогою prompt запитати як звуть користувача. За допомогою alert вивести "Hello, John! How are you?", де John це те, що ввів користувач.
-// // Врахувати, що користувач може натиснути cancel або нічого не ввести
-const userName = prompt("What's your name?");
+// 1 За допомогою prompt запитати як звуть користувача. За допомогою alert вивести "Hello, John! How are you?", де John це те, що ввів користувач.
+// Врахувати, що користувач може натиснути cancel або нічого не ввести
+const userName = prompt('What\'s your name?');
 
-if (userName === undefined || userName === null || userName === '') {
+if (userName === null || userName === '') {
   alert('Hello, anonym!');
 } else {
   alert(`Hello, ${userName}! How are you?`);
 }
 
-// // Створити скрипт для складання, віднімання, множення та поділу двох чисел та виведення результатів.
-// // Числа вводить користувач, врахувати, що він може ввести невалідні дані
+// 2 Створити скрипт для складання, віднімання, множення та поділу двох чисел та виведення результатів.
+// Числа вводить користувач, врахувати, що він може ввести невалідні дані
 const numberOne = prompt('Enter first digit.');
 const numberTwo = prompt('Enter second digit.');
 
-if (!isNaN(numberOne) && !isNaN(numberTwo)) {
+if (
+  !isNaN(numberOne) &&
+  !isNaN(numberTwo) &&
+  numberOne !== null &&
+  numberTwo !== null &&
+  numberOne !== '' &&
+  numberTwo !== ''
+) {
   const mathOperation = prompt('Enter math operation you want to execute.');
 
   let result;
@@ -39,30 +46,37 @@ if (!isNaN(numberOne) && !isNaN(numberTwo)) {
   alert('Please enter valid digits in both fields. Renew page and try again.');
 }
 
-// Створити скрипт, який отримує від користувача (prompt) два значення і виводить (alert) true, якщо значення рівні, false - якщо ні
+// 3 Створити скрипт, який отримує від користувача (prompt) два значення і виводить (alert) true, якщо значення рівні, false - якщо ні
 const equalCheckMessage = alert(
-  'In the next promt enter digits you want to check for equality.'
+  'In the next promt enter some value you want to check for equality.'
 );
-const firstDigit = prompt('Please enter first digit.');
-const secondDigit = prompt('Please enter second digit.');
+const firstValue = prompt('Please enter first value.');
+const secondValue = prompt('Please enter second value.');
 
-if (firstDigit === null || secondDigit === null) {
+if (firstValue === null || secondValue === null) {
   alert('Please enter valid digits in both fields. Renew page and try again.');
 } else {
-  alert(`The digits are equal: ${firstDigit === secondDigit}`);
+  alert(`The digits are equal: ${firstValue === secondValue}`);
 }
 
-// Розкласти по цифрах п'ятизначне число і вивести у вихідному порядку через пробіл (використовувати оператор “розподіл по модулю” %)
-const fifthDigitNumber = prompt('Please enter five digits number.');
-if (!isNaN(fifthDigitNumber) && fifthDigitNumber.length === 5) {
-  let fifthDigit = fifthDigitNumber % 10;
-  let fourthDigit = Math.floor((fifthDigitNumber % 100) / 10);
-  let thirdDigit = Math.floor((fifthDigitNumber % 1000) / 100);
-  let secondDigit = Math.floor((fifthDigitNumber % 10000) / 1000);
-  let firstDigit = Math.floor(fifthDigitNumber / 10000);
+// 4 Розкласти по цифрах п'ятизначне число і вивести у вихідному порядку через пробіл (використовувати оператор “розподіл по модулю” %)
+const fifthDigitNumber = Number(prompt('Please enter five digits number.'));
+
+if (
+  (fifthDigitNumber >= 10000 && fifthDigitNumber <= 99999) ||
+  (fifthDigitNumber <= -10000 && fifthDigitNumber >= -99999)
+) {
+  const number = Math.abs(fifthDigitNumber);
+  const fifthDigit = number % 10;
+  const fourthDigit = Math.floor((number % 100) / 10);
+  const thirdDigit = Math.floor((number % 1000) / 100);
+  const secondDigit = Math.floor((number % 10000) / 1000);
+  const firstDigit = Math.floor(number / 10000);
 
   alert(
-    `${firstDigit} ${secondDigit} ${thirdDigit} ${fourthDigit} ${fifthDigit}`
+    `${
+      fifthDigitNumber < 0 ? '-' : ''
+    } ${firstDigit} ${secondDigit} ${thirdDigit} ${fourthDigit} ${fifthDigit}`
   );
 } else {
   alert('Enter valid five digit number.');
